b3c064d514f890e4cb36c393bbd28f5fdd179fd4
alexis <alexgarca31@gmail.com>
2021-11-08 14:02:26 -0500

    commit

--- src/main/java/classroom/Taller4.java
+++ src/main/java/classroom/Taller4.java
@@ -9,7 +9,7 @@ public class Taller4 {
         estudiantes1[2] = new Persona(71212, "Maria");
         Persona profesor1 = new Persona("Juan", 988);
         Asignatura asignatura1 = new Asignatura("Calculo");
-        Grupo grupo1 = new Grupo(estudiantes1, profesor1, asignatura1, 10023, "M J 2-4");
+        Grupo grupo = new Grupo(estudiantes1, profesor1, asignatura1, 10023, "M J 2-4");
 
         Persona profesor2 = new Persona(99212, "Andrea");
         Asignatura asignatura2 = new Asignatura(882);
